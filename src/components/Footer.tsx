9993f757677cd86cc850f29a2cc73b2cd1ef1668
Ayush Agrawal <ayush.agr254@gmail.com>
2024-09-16 22:02:02 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <p className="m-4 text-sm text-muted-foreground text-center">
       Built by <Link href="https://ayushagr.me"> Ayush Agrawal </Link> • Code on{" "}
       <Link href="https://github.com/A91y/blinks">GitHub</Link> •{" "}
-      <Link className="text-white" href="mailto:ayush@ayushagr.me">
+      <Link className="text-white" href="mailto:hello@ayushagr.me">
         {" "}
         Contact by ✉️
       </Link>
