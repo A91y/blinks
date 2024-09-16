6d013db9d61df3a8a0fe6267a2c1da8dee0f69ba
A91y <65825207+A91y@users.noreply.github.com>
2024-09-16 16:25:41 +0530

    fix: finally centered the div

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Title = () => {
   return (
     <div className="flex justify-center items-center mt-8 mb-12">
-      <h1 className="text-6xl font-bold">Blinks by <a href="https://ayushagr.me" className="text-blue-700" target="_blank">Ayush</a></h1>
+      <h1 className="text-6xl font-bold px-4">
+        Blinks by{" "}
+        <a href="https://ayushagr.me" className="text-blue-700" target="_blank">
+          Ayush
+        </a>
+      </h1>
     </div>
   );
 };
