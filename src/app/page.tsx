33098a318eb2f827f2a4a836a58dfe611cf7a080
A91y <65825207+A91y@users.noreply.github.com>
2024-09-16 16:31:50 +0530

    Refactor page layout and add divider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Title from "@/components/Title";
 import Cards from "@/components/Cards";
 import Footer from "@/components/Footer";
+import { Divider } from "@nextui-org/divider";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col items-center min-h-screen">
+      <div className="flex flex-col items-center min-h-[93vh]">
         <Title />
         <Cards />
       </div>
+      <Divider className="mt-6 md:mt-2"/>
       <Footer />
     </div>
   );
